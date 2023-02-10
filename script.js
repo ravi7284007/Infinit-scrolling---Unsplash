50f457c2e2ed21ddb6d9ed8b84d076cf581f7cb4
ravi7284007 <ravi7284007@gmail.com>
2023-02-10 05:45:11 +0530

    changes in script file

--- script.js
+++ script.js
@@ -13,20 +13,34 @@ const APIKEY = `tHoYMbjiQBDLML3U45fy_QapwcU2-fqGV3-R4vy_L_o`;
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${APIKEY}&count=${count}&orientation=${view}`;
 
+function setAttribute(ele, attr) {
+    for (const key in attr) {
+        ele.setAttribute(key, attr[key])
+    }
+}
+
 // Create Element for links & photos, Add to the DOM
 function displayPhotos() {
     photoArray.forEach(photo => {
         // Create A tag to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
         item.classList.add('imageBox')
-
+        setAttribute(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description)
-        img.setAttribute('title', photo.alt_description);
-
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description)
+        // img.setAttribute('title', photo.alt_description);
+        setAttribute(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+        console.log(photo);
         item.appendChild(img);
         imageContainer.appendChild(item)
     })
